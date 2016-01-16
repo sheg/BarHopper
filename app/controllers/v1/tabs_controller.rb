d3a261c64c6b1134b63d1f64d0cb926e5abc825b
dforkosh <dforkosh@salesforce.com>
2016-01-15 23:21:51 -0500

    implement tabs index and show actions

--- app/controllers/v1/tabs_controller.rb
+++ app/controllers/v1/tabs_controller.rb
@@ -1,6 +1,26 @@
 module V1
   class TabsController < ApplicationController
     before_action :authenticate
+    before_action :get_user
+
+    def index
+      render json: @user.tabs, status: :ok
+    end
+
+    def show
+      tab = @user.tabs.find_by(id: params[:id])
+      if tab
+        render json: tab, status: :ok
+      else
+        render json: "tab not found", status: :not_found
+      end
+    end
+
+    protected
+
+      def get_user
+        @user = User.find_by(id: params[:user_id])
+      end
 
   end
 end
